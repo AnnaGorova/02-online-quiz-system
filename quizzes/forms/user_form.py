4257f12e7e043b4c667ab9a3b766d8e81d559bde
Поліна Єремій <yeremiipolina@gmail.com>
2026-05-29 19:09:36 +0300

    останні зміни

--- quizzes/forms/user_form.py
+++ quizzes/forms/user_form.py
@@ -1,12 +1,9 @@
 from django import forms
 
+
 class UsernameForm(forms.Form):
     username = forms.CharField(
         max_length=100,
         label="Ваше ім'я",
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введіть ваше ім\'я',
-            'required': True
-        })
+
     )
